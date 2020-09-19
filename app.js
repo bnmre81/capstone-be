86e2d19245795fa9df78d6cc1bfd35b4bf264ba7
Hassan Alwayel <hsn.alwayel@gmail.com>
2020-09-19 22:37:20 +0300

    socket Io setup

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
-
 const app = express();
+const http = require("http");
+const path = require("path");
+const socketIO = require("socket.io");
 
-app.get("/", (req, res) => {
-  console.log("HELLO");
+//Creating a server
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on("connection", (socket) => {
+  console.log("A new user just connected");
 });
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.log(`The application is running on ${port}`);
 });
